ac9e1ee90a8e246fe791791ba33e27aecfe89452
KatrinaZuus <katrinazuus@gmail.com>
2024-02-27 01:16:30 +0400

    js

--- script.js
+++ script.js
@@ -1,24 +1,41 @@
 
-const launchDay = Number(document.getElementById("day").innerText)
-const launchHours = Number(document.getElementById("hours").innerText)
-const launchMinuts = Number(document.getElementById("minuts").innerText)
-const launchSeconds = Number(document.getElementById("seconds").innerText)
+// const launchDay = Number(document.getElementById("day").innerText)
+// const launchHours = Number(document.getElementById("hours").innerText)
+// const launchMinuts = Number(document.getElementById("minuts").innerText)
+// const launchSeconds = Number(document.getElementById("seconds").innerText)
 
-console.log (launchDay, launchHours, launchMinuts, launchSeconds)
+// console.log (launchDay, launchHours, launchMinuts, launchSeconds)
 
-let countDownDate = new Date("Feb 26, 2024 00:45:25").getTime();
-console.log(countDownDate)
-let now = new Date().getTime();
-console.log(now)
-distance = countDownDate - now;
-console.log(distance)   
-var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-console.log(days)
-var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-console.log(hours)
-var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-console.log(minutes)
-var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-console.log(seconds)
+// 777341000
+let countDownDate = new Date().getTime() + 777341000;
+// console.log (countDownDate)
+
+let x = setInterval(function(){
+
+  // Get today's date and time
+  let now = new Date().getTime();
+//   console.log(now)
+
+  var distance = countDownDate - now;
+  console.log(distance) 
+    
+
+  var launchDay = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var launchHours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var launchMinuts = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var launchSeconds = Math.floor((distance % (1000 * 60)) / 1000);
+    
+document.getElementById("day").innerText = launchDay
+document.getElementById("hours").innerText = launchHours
+document.getElementById("minuts").innerText = launchMinuts
+document.getElementById("seconds").innerText = launchSeconds
+
+
+
+  if (distance <= 1000) {
+    clearInterval(x);
+  }
+}, 1000);
+
  
 
