f632e2bc0900a0a7d0dc577701e4226c0d37a0d0
KatrinaZuus <katrinazuus@gmail.com>
2024-02-28 22:20:24 +0400

    js sxvagzit gavwere

--- script.js
+++ script.js
@@ -7,44 +7,84 @@
 // console.log (launchDay, launchHours, launchMinuts, launchSeconds)
 
 // 777341000
-let countDownDate = new Date().getTime() + 3660000;
+// let countDownDate = new Date().getTime() + 3660000;
 // console.log (countDownDate)
 
-let x = setInterval(function(){
+// let seconds = 1000
+// let minutes = seconds * 60
+// let hours = minutes * 60
+// let day = hours * 24
+
+// let x = setInterval(function(){
 
   // Get today's date and time
-  let now = new Date().getTime();
+  // let now = new Date().getTime();
 //   console.log(now)
 
-  var distance = countDownDate - now;
-  console.log(distance) 
+  // var distance = countDownDate - now;
+  // console.log(distance) 
 
-let seconds = 1000
-let minutes = seconds * 60
-let hours = minutes * 60
-let day = hours * 24
-
-
-  var launchDay = Math.floor(distance / day);
-  console.log(launchDay)
-  var launchHours = Math.floor((distance % day) / hours);
-  console.log(launchHours)
-  var launchMinuts = Math.floor((distance % hours) / minutes);
-  console.log(launchMinuts)
-  var launchSeconds = Math.floor((distance % minutes) / seconds);
-  console.log(launchSeconds)
+//   var launchDay = Math.floor(distance / day);
+//   console.log(launchDay)
+//   var launchHours = Math.floor((distance % day) / hours);
+//   console.log(launchHours)
+//   var launchMinuts = Math.floor((distance % hours) / minutes);
+//   console.log(launchMinuts)
+//   var launchSeconds = Math.floor((distance % minutes) / seconds);
+//   console.log(launchSeconds)
     
-document.getElementById("day").innerText = launchDay
-document.getElementById("hours").innerText = launchHours
-document.getElementById("minuts").innerText = launchMinuts
-document.getElementById("seconds").innerText = launchSeconds
+// document.getElementById("day").innerText = launchDay
+// document.getElementById("hours").innerText = launchHours
+// document.getElementById("minuts").innerText = launchMinuts
+// document.getElementById("seconds").innerText = launchSeconds
 
 
 
-  if (distance <= 1000) {
-    clearInterval(x);
-  }
-}, 1000);
+//   if (distance <= 1000) {
+//     clearInterval(x);
+  // }
+// }, 1000);
 
  
 
+let day = Number(document.getElementById("day").innerText)
+let hours = Number(document.getElementById("hours").innerText)
+let minuts = Number(document.getElementById("minuts").innerText)
+let seconds = Number(document.getElementById("seconds").innerText)
+console.log (day, hours, minuts, seconds)
+
+// const day = 
+// const hours = 23
+// const minuts = 55
+// const seconds = 24
+
+x = setInterval(()=>{
+
+  if (day == 0 && hours == 0 && minuts == 0 && seconds ==0) {
+      clearInterval (x)
+    }
+    if (hours == 0 && day>0){ 
+      day--
+      hours = 23
+    }
+    if (minuts == 0 && hours >0){
+      minuts = 59
+      hours--
+    }
+    if (seconds == 0 && minuts >0) {
+      seconds = 59
+      minuts--
+    }   seconds--
+  
+  
+
+
+
+  document.getElementById("day").innerText = day
+  document.getElementById("hours").innerText = hours
+  document.getElementById("minuts").innerText = minuts
+  document.getElementById("seconds").innerText = seconds
+
+
+
+}, 1000)
